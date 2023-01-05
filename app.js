56d6f2d6cfc6951d25f09ef2ead4d8b4768e7750
sreyes25 <82613689+sreyes25@users.noreply.github.com>
2023-01-04 19:34:11 -0500

    test1
    testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const NotesRoutes = require('./routes/notesRoutes');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const send = require('./sms');
 
 //Envirenment
